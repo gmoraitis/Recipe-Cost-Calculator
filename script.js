c44986bb2f923dd60d8582e2a1f7a22896844422
George Moraitis <georgemoraitis@Georges-MacBook-Pro.local>
2021-10-27 22:21:51 +0200

    Studing  for refactor Javascript with loops

--- script.js
+++ script.js
@@ -24,7 +24,18 @@ function result() {
     
 }
 
+var table = document.getElementById("mytab1");
+for (var i = 0, row; row = table.rows[i]; i++) {
+   //iterate through rows
+   //rows would be accessed using the "row" variable assigned in the for loop
 
+   console.log(table.rows[i])
+   for (var j = 0, col; col = row.cells[j]; j++) {
+     //iterate through columns
+     //columns would be accessed using the "col" variable assigned in the for loop
+     
+   }  
+}
 
 // updateSubTotal(); // Initial call
 
