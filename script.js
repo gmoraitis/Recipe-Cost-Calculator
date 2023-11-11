3c78b7aba8e0eea148e92ea2ee82ef30012ab4cb
George Moraitis <40567588+gmoraitis@users.noreply.github.com>
2023-11-11 14:35:42 +0100

    Added more rows

--- script.js
+++ script.js
@@ -50,6 +50,86 @@ function calc6() {
     parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
 }
 
+function calc7() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
+function calc8() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
+function calc9() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
+function calc10() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
+function calc11() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
+function calc12() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
+function calc13() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
+function calc14() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
+function calc15() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
+function calc16() {
+    
+    var pricePerKilos22 = parseFloat(document.getElementById('22').value);
+    var gramPerPortion23 = parseFloat(document.getElementById('23').value);
+    var pricePerPortion24 = document.getElementById('24');
+    parseFloat(pricePerPortion24.value = gramPerPortion23 / kilo * pricePerKilos22);
+}
+
 
 function result() {
  
@@ -77,7 +157,47 @@ function result() {
     console.log(sum6)
     sum6 = sum6 || 0
 
-    var x = sum1 + sum2 + sum3 + sum4 + sum5 + sum6;
+    var sum7 = parseFloat(document.getElementById('28').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+
+    var sum8 = parseFloat(document.getElementById('32').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+    
+    var sum9 = parseFloat(document.getElementById('36').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+
+    var sum10 = parseFloat(document.getElementById('40').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+
+    var sum11 = parseFloat(document.getElementById('44').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+
+    var sum12 = parseFloat(document.getElementById('48').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+
+    var sum13 = parseFloat(document.getElementById('52').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+
+    var sum14 = parseFloat(document.getElementById('56').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+
+    var sum15 = parseFloat(document.getElementById('60').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+
+    var sum16 = parseFloat(document.getElementById('64').value);
+    console.log(sum6)
+    sum6 = sum6 || 0
+
+    var x = sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8 + sum9 + sum10 + sum11 + sum12 + sum13 + sum14 + sum15 + sum16;
     var fixed = x.toFixed(2);
     var y = document.getElementById('result');
     y.innerText = fixed;
